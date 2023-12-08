9197770637a93b0eff0579d675bc3e8da50196e4
Mahendrakoppula <mahendrakoppula09@gamil.com>
2023-12-08 16:20:18 +0530

    dev code

---
+++ src/models/SubFolderPanchayithe/SubFolder.model.ts
@@ -0,0 +1,36 @@
+// models/Album.js
+import mongoose from 'mongoose';
+const currentDate = new Date();
+const subchema = new mongoose.Schema({
+    SubFolderName: {
+    type: String,
+    required: true,
+  },
+
+  MainmostFolderName: {
+    type: String,
+    required: true,
+  },
+
+  audiomessagessubfolder: {
+    type: String,
+    required: true,
+  },
+  SubFolderkey: {
+    type: String,
+    required: true,
+  },
+  SubFolder_banner: {
+    type: String,
+    required: true,
+  },
+
+  createdAt: {
+    type: Date,
+   default: currentDate,
+  },
+});
+
+const SubFolder = mongoose.model('SubFolder', subchema);
+
+export default SubFolder;
